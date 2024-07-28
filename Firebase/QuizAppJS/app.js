e775541f070de650eaef02b2e7d7d0716851fe46
Aliza Khan <alizaadi2@gmail.com>
2024-07-29 01:13:23 +0500

    Update app.js

--- Firebase/QuizAppJS/app.js
+++ Firebase/QuizAppJS/app.js
@@ -1,1 +1,23 @@
 // JS WORK REMAINING
+import { auth, onAuthStateChanged, signOut} from "./firebase.js"
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+    //   const uid = user.uid;
+    console.log("user id", user);
+      // ...
+    } else {
+      // User is signed out
+      console.log("user not login ");
+      // ...
+    }
+  });
+  
+
+
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
