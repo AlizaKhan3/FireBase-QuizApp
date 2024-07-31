94af45ca611f5f2089fe2124023f5b3f4a9aead1
Aliza Khan <alizaadi2@gmail.com>
2024-07-31 23:35:31 +0500

    Update app.js

--- Firebase/QuizAppJS/app.js
+++ Firebase/QuizAppJS/app.js
@@ -1,23 +1,37 @@
 // JS WORK REMAINING
-import { auth, onAuthStateChanged, signOut} from "./firebase.js"
+import { auth, onAuthStateChanged, signOut } from "./firebase.js"
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
     //   const uid = user.uid;
     console.log("user id", user);
-      // ...
-    } else {
-      // User is signed out
-      console.log("user not login ");
-      // ...
-    }
-  });
-  
+    // ...
+  } else {
+    // User is signed out
+    console.log("user not login ");
+    // ...
+  }
+});
 
 
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+
+const username = document.getElementById("username");
+const startScreen = document.querySelector(".startScreen");
+const endScreen = document.querySelector(".endScreen");
+const playground = document.querySelector(".playground");
+const questionNumber = document.querySelector(".questionNumber");
+const timer = document.getElementById("timer");
+const options = document.getElementById("options");
+const question = document.getElementById("question");
+const quizBody = document.querySelector(".quizBody");
+const loader = document.querySelector(".loader");
+const finalScore = document.querySelector(".finalScore");
+const resultUsername = document.querySelector(".resultUsername");
+
+
