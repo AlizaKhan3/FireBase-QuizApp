681c1f7012e04ade9748c880c1d77cf1c378b4ac
Aliza Khan <alizaadi2@gmail.com>
2024-08-01 01:26:46 +0500

    Update app.js

--- Firebase/QuizAppJS/app.js
+++ Firebase/QuizAppJS/app.js
@@ -36,21 +36,57 @@ const resultUsername = document.querySelector(".resultUsername");
 let start = document.getElementById("start");
 start.addEventListener("click", startQuiz);
 
+let questionIndex = 0;
 let arrayQuestion = [];
 let score = [];
 let count = 30;
 
-function startQuiz(){
-  if (username.value !== ""){
+function startQuiz() {
+  if (username.value !== "") {
     console.log(username.value);
     startScreen.style.display = "none";
     playground.style.display = "block";
-  }else{
+    loader.style.display = "block";
+
+    loadQuestions();
+  } else {
     username.style.border = "0.5px solid red"
     username.style.boxShadow = "0 0 10px rgba(255, 0, 0, 0.5)"
   }
 }
 
-function loadQuestions(){
+// using quiz api "opentdb-trivia"
+let QUIZ_URL = "https://opentdb.com/api.php?amount=10&category=18&type=multiple"
+function loadQuestions() {
+  fetch(QUIZ_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
+      arrayQuestion = data.results;
+      displayQuestions(arrayQuestion[questionIndex]);
+    });
+}
+
+function displayQuestions(questionData) {
+  console.log(questionData);
+  question.innerHTML = questionData.question;
+  // questionNumber.innerHTML = questionIndex ++;
+  loadOptions(questionData);
+}
+
+function loadOptions(questionData) {
+  options.innerHTML = " ";
+  let correctAnswer = questionData.correct_answer;
+  let incorrectAnswers = questionData.incorrect_answers;
   
+  // Combine correct and incorrect answers into a single array
+  let answers = [...incorrectAnswers];
+  
+  // Insert the correct answer at a random index
+  let randomIndex = Math.floor(Math.random() * (incorrectAnswers.length + 1));
+  answers.splice(randomIndex, 0, correctAnswer);
+  
+  console.log(answers);
+
+  loader.style.display = "none";
 }
